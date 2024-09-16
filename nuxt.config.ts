0fdaf0c07bdeec3a15cc3fc20c60e8a4b85164b9
GabrielSpookyDev <gabrielicmusa@gmail.com>
2024-09-16 16:15:52 -0300

    feat: added nuxtUI and vue transitions to project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+
+  modules: [
+    "@nuxt/ui",
+    /* The above also includes: 
+    @nuxt/icon
+    @nuxtjs/tailwindcss
+    @nuxtjs/color-mode
+    */
+    "@morev/vue-transitions/nuxt",
+  ],
+});
---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "tailwindcss";
+
+export default <Partial<Config>>{
+  theme: {
+    extend: {
+      colors: {
+        "betpass-lightgrey": "1E1E1E",
+        "betpass-grey": "1A212A",
+      },
+    },
+  },
+};

761ad608ea8c23e892c75e82d9e6ebaf3af9a1fe
GabrielSpookyDev <gabrielicmusa@gmail.com>
2024-09-17 21:02:30 -0300

    feat: createDealsSidebar now using baseDealsSidebar component

--- components/createDealsSidebar.vue
+++ components/createDealsSidebar.vue
@@ -1,88 +1,59 @@
 <template>
-  <div>
-    <!-- Sidebar Content -->
-    <TransitionSlide :offset="['100%', 0]">
-      <div
-        v-if="sidebarOpen"
-        class="sidebar fixed inset-y-0 right-0 bg-betpass-lightgrey p-12 my-[6px] border border-black rounded-l-2xl"
-      >
-        <div class="font-bold pr-16">
-          <p v-if="!editingDeal" class="text-2xl mt-8">Criar Deal</p>
-          <p v-else class="text-2xl mt-8">Editar Deal</p>
+  <BaseDealsSidebar :sidebar-open="sidebarOpen">
+    <template #sidebarContent>
+      <div class="font-bold pr-16">
+        <p class="text-2xl mt-8">Criar Deal</p>
 
-          <p>Editing Deal: {{ editingDeal }}</p>
+        <div class="mt-12">
+          <div>
+            <p class="mb-3">Nome da casa</p>
+            <input
+              class="input w-full"
+              v-model="houseName"
+              placeholder="Exemplo: ABC BET"
+              type="text"
+            />
+          </div>
 
-          <div class="mt-12">
-            <div>
-              <p class="mb-3">Nome da casa</p>
-              <input
-                class="input w-full"
-                v-model="houseName"
-                placeholder="Exemplo: ABC BET"
-                type="text"
-              />
-            </div>
+          <div class="mt-6">
+            <p class="mb-3">Descrição</p>
+            <input
+              class="input w-full"
+              v-model="description"
+              placeholder="Descrição do deal"
+              type="text"
+            />
+          </div>
 
-            <div class="mt-6">
-              <p class="mb-3">Descrição</p>
-              <input
-                class="input w-full"
-                v-model="description"
-                placeholder="Descrição do deal"
-                type="text"
-              />
-            </div>
+          <div class="mt-6">
+            <p class="mb-3">Nota</p>
+            <select class="input" v-model="grade">
+              <option v-for="value in houseValues" :key="value" :value="value">
+                {{ value }}
+              </option>
+            </select>
+          </div>
 
-            <div class="mt-6">
-              <p class="mb-3">Nota</p>
-              <select class="input" v-model="grade">
-                <option
-                  v-for="value in houseValues"
-                  :key="value"
-                  :value="value"
-                >
-                  {{ value }}
-                </option>
-              </select>
-            </div>
-
-            <div class="mt-11 flex items-center">
-              <UToggle v-model="soldOut" />
-              <p class="ml-4">Esgotar deal</p>
-            </div>
+          <div class="mt-11 flex items-center">
+            <UToggle v-model="soldOut" />
+            <p class="ml-4">Esgotar deal</p>
           </div>
         </div>
-
-        <div class="flex mt-[74px] font-bold">
-          <button class="button text-[12px]" @click="resetAndToggleSidebar">
-            Cancelar
-          </button>
-          <button
-            v-if="!editingDeal"
-            class="button text-[12px] bg-betpass-green text-betpass-dark-green rounded-3xl"
-            @click="createDeal"
-          >
-            Salvar
-          </button>
-          <button
-            v-else
-            class="button text-[12px] bg-betpass-green text-betpass-dark-green rounded-3xl"
-            @click="editDeal()"
-          >
-            Edit
-          </button>
-        </div>
       </div>
-    </TransitionSlide>
 
-    <!-- Overlay -->
-    <TransitionFade>
-      <div
-        v-if="sidebarOpen"
-        class="overlay fixed inset-0 bg-betpass-grey opacity-[.81] w-full"
-      ></div>
-    </TransitionFade>
-  </div>
+      <div class="flex mt-[74px] font-bold">
+        <button class="button text-[12px]" @click="resetAndToggleSidebar">
+          Cancelar
+        </button>
+        <button
+          class="button text-[12px] bg-betpass-green text-betpass-dark-green rounded-3xl"
+          @click="createDeal"
+        >
+          Salvar
+        </button>
+      </div>
+    </template>
+  </BaseDealsSidebar>
 </template>
 
 <script setup lang="ts">
@@ -91,28 +62,13 @@ import { useCardStore } from "~/store/useDataStore";
 
 const cardStore = useCardStore();
 
-interface Card {
-  house: string;
-  grade: number | "";
-  soldOut: boolean;
-  createdDate: string;
-}
-
 interface Props {
   sidebarOpen: boolean;
-  dealIndex: number;
 }
 
 const props = defineProps<Props>();
 
 const emit = defineEmits(["update:sidebarOpen"]);
-
-const editingDeal = computed(() => {
-  if (cardStore.editingDeal && props.dealIndex) {
-    return true;
-  }
-  return false;
-});
 
 const houseName = ref("");
 const description = ref("");
@@ -149,14 +105,9 @@ function createDeal() {
   resetAndToggleSidebar();
 }
 
-function editDeal(cardIndex: number, updatedData: Partial<Card>) {
-  cardStore.modifyCard(cardIndex, updatedData);
-}
-
 function resetAndToggleSidebar() {
   resetValues();
   toggleSidebar();
-  cardStore.editingDeal = false;
 }
 
 function resetValues() {
@@ -187,15 +138,5 @@ function toggleSidebar() {
 
 .input {
   @apply bg-betpass-lightgrey rounded-lg border border-white py-3 px-5 text-sm;
-}
-
-.sidebar {
-  max-width: 40%;
-  width: 100%;
-  z-index: 20;
-}
-
-.overlay {
-  z-index: 10;
 }
 </style>

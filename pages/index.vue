6cc6c3a424017b7a2de59d0dbccf7e897218dfae
GabrielSpookyDev <gabrielicmusa@gmail.com>
2024-09-18 02:00:09 -0300

    feat: deals now paginated

--- pages/index.vue
+++ pages/index.vue
@@ -5,9 +5,9 @@
       <h1 class="text-2xl text-betpass-green font-bold">DEALS</h1>
       <button @click="toggleCreateDealsSidebar()">
         <div class="bg-betpass-green rounded-3xl py-[10px] px-[34px]">
-          <span class="text-betpass-dark-green font-bold text-sm"
-            >CRIAR DEALS</span
-          >
+          <span class="text-betpass-dark-green font-bold text-sm">
+            CRIAR DEALS
+          </span>
         </div>
       </button>
     </div>
@@ -29,20 +29,40 @@
 
     <!-- Cards Section -->
     <div class="mt-16">
-      <CardsSection :cardsData="filteredCardsData" />
+      <CardsSection :cardsData="paginatedCardsData" />
+    </div>
+
+    <!-- Pagination Controls -->
+    <div class="flex justify-center">
+      <button
+        v-for="page in totalPages"
+        :key="page"
+        :class="[
+          'mx-1 h-[30px] w-[30px] text-black font-bold rounded-lg',
+          {
+            'bg-betpass-green': currentPage === page,
+            'bg-gray-200': currentPage !== page,
+          },
+        ]"
+        @click="goToPage(page)"
+      >
+        {{ page }}
+      </button>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, computed, onMounted } from "vue";
+import { ref, computed, onMounted, watch } from "vue";
 import { useCardStore } from "~/store/useDataStore";
 
 const cardStore = useCardStore();
 
 const searchInput = ref("");
-
 const createDealsSidebarOpen = ref(false);
+
+const currentPage = ref(1);
+const itemsPerPage = 6;
 
 function toggleCreateDealsSidebar() {
   createDealsSidebarOpen.value = !createDealsSidebarOpen.value;
@@ -53,6 +73,26 @@ const filteredCardsData = computed(() => {
   return cardStore.cardData.filter((card) =>
     card.house.toLowerCase().includes(searchTerm)
   );
+});
+
+const paginatedCardsData = computed(() => {
+  const startIndex = (currentPage.value - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  return filteredCardsData.value.slice(startIndex, endIndex);
+});
+
+const totalPages = computed(() =>
+  Math.ceil(filteredCardsData.value.length / itemsPerPage)
+);
+
+function goToPage(page: number) {
+  if (page >= 1 && page <= totalPages.value) {
+    currentPage.value = page;
+  }
+}
+
+watch(searchInput, () => {
+  currentPage.value = 1;
 });
 
 onMounted(() => {
